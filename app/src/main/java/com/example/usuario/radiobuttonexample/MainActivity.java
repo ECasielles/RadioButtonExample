c5de1dd79d4c5d0520ed32146bc021cbf7078f32
ECasielles <flapspoiler@gmail.com>
2017-12-01 13:59:14 +0100

    Event update

--- app/src/main/java/com/example/usuario/radiobuttonexample/MainActivity.java
+++ app/src/main/java/com/example/usuario/radiobuttonexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.usuario.radiobuttonexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
 
 /**
  * Clase de ejemplo para RadioButton y RadioGroup
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private View constraintProfessional;
     private View constraintBusiness;
+    private RadioGroup rgpOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +23,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         constraintProfessional = findViewById(R.id.constraintProfessional);
         constraintBusiness = findViewById(R.id.constraintBusiness);
+        rgpOptions = findViewById(R.id.rgpOptions);
+        rgpOptions.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int viewId) {
+                switch (viewId) {
+                    case R.id.rbtBusiness:
+                        viewProfessional(false);
+                        break;
+                    case R.id.rbtProfessional:
+                        viewProfessional(true);
+                        break;
+                }
+            }
+        });
     }
-
-    /**
+    /*/**
      * Método que gestiona el evento onClick de los componentes RadioButton
-     * @param view Objeto RadioButton activado.
-     */
+     * @param view Objeto RadioButton activado.*/
+    /*
     public void onRadioButtonClicked (View view){
         switch (view.getId()){
             case R.id.rbtBusiness:
-                viewProfessional(true);
-                break;
-            case R.id.rbtProfessional:
                 viewProfessional(false);
                 break;
+            case R.id.rbtProfessional:
+                viewProfessional(true);
+                break;
         }
-    }
+    }*/
 
     /**
      * Método que indica qué ConstraintLayout ha de estar visible según la opción seleccionada
